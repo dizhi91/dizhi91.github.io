b8d32204a5b67332e3b3952c1a8601355562a565
dizhi91 <66222966+dizhi91@users.noreply.github.com>
2020-06-30 00:29:15 +0200

    Update publish.js

--- publish.js
+++ publish.js
@@ -2,8 +2,8 @@ var emails = [
     'dizhi9191@gmail.com'
 ];
 var newestUrls = [
+    'https://jiuse.icu',
     'https://91porny.com',
-    //'https://vt4.xyz',
 ];
 var otherUrls = [
     'https://dizhi91.github.io',
@@ -11,7 +11,7 @@ var otherUrls = [
 ];
 var foreverUrls = [
     'https://91porny.com',
-];
+];https://jiuse.icu
 var notices = [
     '* 我们推荐PC和Andriod手机用户使用Chrome(谷歌)浏览器访问，iPhone用户我们建议您使用手机自带Safria浏览器访问。',
     '* 为了防止域名劫持而无法访问，请设置手机和PC的DNS，我们推荐使用8.8.8.8和1.1.1.1。',

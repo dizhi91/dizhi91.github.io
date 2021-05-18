b877bbd41ddab09c0d20725c2702456bd229ecc9
91porny <66222966+91porny@users.noreply.github.com>
2021-05-18 23:42:27 +0200

    add 802

--- publish.js
+++ publish.js
@@ -3,8 +3,8 @@ var emails = [
 ];
 var newestUrls = [
     'https://jiuse911.com',
-	'https://jiuse801.xyz',
-	'https://jiuse800.xyz'
+	'https://jiuse802.xyz',
+	'https://jiuse801.xyz'
 ];
 var otherUrls = [
     'https://jiuse.org',
